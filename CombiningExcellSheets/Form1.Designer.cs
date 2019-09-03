fbf2366f8411c0d3e11a8ba4b01d088965d47cb5
Efecan Altay <efecan.altay@idesse.com.tr>
2019-09-03 14:29:03 +0300

    Add project files.

---
+++ CombiningExcellSheets/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace CombiningExcellSheets
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_loadExcells = new System.Windows.Forms.Button();
+            this.btn_join = new System.Windows.Forms.Button();
+            this.listbox_excellPaths = new System.Windows.Forms.ListBox();
+            this.btn_select_out = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstbox_outPath = new System.Windows.Forms.ListBox();
+            this.lstBox_output = new System.Windows.Forms.ListBox();
+            this.btn_show_totalRowSize = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_rowCount = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_loadExcells
+            // 
+            this.btn_loadExcells.Location = new System.Drawing.Point(12, 12);
+            this.btn_loadExcells.Name = "btn_loadExcells";
+            this.btn_loadExcells.Size = new System.Drawing.Size(92, 43);
+            this.btn_loadExcells.TabIndex = 0;
+            this.btn_loadExcells.Text = "Excell Yükle";
+            this.btn_loadExcells.UseVisualStyleBackColor = true;
+            this.btn_loadExcells.Click += new System.EventHandler(this.Btn_loadExcells_Click);
+            // 
+            // btn_join
+            // 
+            this.btn_join.Location = new System.Drawing.Point(8, 269);
+            this.btn_join.Name = "btn_join";
+            this.btn_join.Size = new System.Drawing.Size(325, 40);
+            this.btn_join.TabIndex = 1;
+            this.btn_join.Text = "Birleştir";
+            this.btn_join.UseVisualStyleBackColor = true;
+            this.btn_join.Click += new System.EventHandler(this.Btn_join_Click);
+            // 
+            // listbox_excellPaths
+            // 
+            this.listbox_excellPaths.FormattingEnabled = true;
+            this.listbox_excellPaths.Location = new System.Drawing.Point(12, 73);
+            this.listbox_excellPaths.Name = "listbox_excellPaths";
+            this.listbox_excellPaths.Size = new System.Drawing.Size(254, 108);
+            this.listbox_excellPaths.TabIndex = 2;
+            this.listbox_excellPaths.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Listbox_excellPaths_KeyDown);
+            // 
+            // btn_select_out
+            // 
+            this.btn_select_out.Location = new System.Drawing.Point(236, 12);
+            this.btn_select_out.Name = "btn_select_out";
+            this.btn_select_out.Size = new System.Drawing.Size(90, 43);
+            this.btn_select_out.TabIndex = 3;
+            this.btn_select_out.Text = "Çıktının Konumunu seç";
+            this.btn_select_out.UseVisualStyleBackColor = true;
+            this.btn_select_out.Click += new System.EventHandler(this.Btn_select_out_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Out Path :";
+            // 
+            // lstbox_outPath
+            // 
+            this.lstbox_outPath.BackColor = System.Drawing.SystemColors.Menu;
+            this.lstbox_outPath.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstbox_outPath.FormattingEnabled = true;
+            this.lstbox_outPath.HorizontalScrollbar = true;
+            this.lstbox_outPath.Location = new System.Drawing.Point(12, 211);
+            this.lstbox_outPath.Name = "lstbox_outPath";
+            this.lstbox_outPath.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.lstbox_outPath.Size = new System.Drawing.Size(321, 52);
+            this.lstbox_outPath.TabIndex = 8;
+            // 
+            // lstBox_output
+            // 
+            this.lstBox_output.BackColor = System.Drawing.SystemColors.Menu;
+            this.lstBox_output.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstBox_output.FormattingEnabled = true;
+            this.lstBox_output.HorizontalScrollbar = true;
+            this.lstBox_output.Location = new System.Drawing.Point(8, 315);
+            this.lstBox_output.Name = "lstBox_output";
+            this.lstBox_output.Size = new System.Drawing.Size(321, 52);
+            this.lstBox_output.TabIndex = 9;
+            // 
+            // btn_show_totalRowSize
+            // 
+            this.btn_show_totalRowSize.Location = new System.Drawing.Point(277, 103);
+            this.btn_show_totalRowSize.Name = "btn_show_totalRowSize";
+            this.btn_show_totalRowSize.Size = new System.Drawing.Size(56, 78);
+            this.btn_show_totalRowSize.TabIndex = 10;
+            this.btn_show_totalRowSize.Text = "Satır Boyutu Hesapla";
+            this.btn_show_totalRowSize.UseVisualStyleBackColor = true;
+            this.btn_show_totalRowSize.Click += new System.EventHandler(this.Btn_show_totalRowSize_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(272, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Toplam Satır";
+            // 
+            // lbl_rowCount
+            // 
+            this.lbl_rowCount.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_rowCount.Location = new System.Drawing.Point(275, 78);
+            this.lbl_rowCount.Name = "lbl_rowCount";
+            this.lbl_rowCount.Size = new System.Drawing.Size(63, 22);
+            this.lbl_rowCount.TabIndex = 12;
+            this.lbl_rowCount.Text = "---";
+            this.lbl_rowCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(111, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 42);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Excell Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 404);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbl_rowCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_show_totalRowSize);
+            this.Controls.Add(this.lstBox_output);
+            this.Controls.Add(this.lstbox_outPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_select_out);
+            this.Controls.Add(this.listbox_excellPaths);
+            this.Controls.Add(this.btn_join);
+            this.Controls.Add(this.btn_loadExcells);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Excell Birleştirme";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_loadExcells;
+        private System.Windows.Forms.Button btn_join;
+        private System.Windows.Forms.ListBox listbox_excellPaths;
+        private System.Windows.Forms.Button btn_select_out;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstbox_outPath;
+        private System.Windows.Forms.ListBox lstBox_output;
+        private System.Windows.Forms.Button btn_show_totalRowSize;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_rowCount;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CombiningExcellSheets/Form1.cs
@@ -0,0 +1,346 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Application = Microsoft.Office.Interop.Excel.Application;
+using DataTable = System.Data.DataTable;
+
+namespace CombiningExcellSheets
+{
+    public partial class Form1 : Form
+    {
+        public class ExcellFile
+        {
+            public string FileName = "";
+            public string Path = "";
+            public int RowSize = 0;
+
+            public override string ToString()
+            {
+                if (RowSize != 0)
+                    return FileName + " (rw_size : " + RowSize + ")";
+                else
+                    return FileName;
+            }
+        }
+
+        public ExcellFile outFile;
+        public int Row_Count = 0;
+
+        ObservableCollection<ExcellFile> excellFilePaths = new ObservableCollection<ExcellFile>();
+        public Form1()
+        {
+            InitializeComponent();
+            excellFilePaths = new ObservableCollection<ExcellFile>();
+        }
+
+        private void Btn_loadExcells_Click(object sender, EventArgs e)
+        {
+            GetOpenAndFileType("Excell Dosyası |*.xlsx");
+        }
+
+        private void Btn_join_Click(object sender, EventArgs e)
+        {
+            ClearConsole();
+            
+            if (outFile == null)
+            {
+                MessageBox.Show("Çıkış Konumu Seçmediniz !!", "Eksik Giriş", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (excellFilePaths.Count > 0)
+            {
+                ReadAndJoinExcells();
+            }
+            else
+            {
+                MessageBox.Show("Hiç Bir Excell Girişi yapılmadı!", "Eksik Giriş", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private async Task Run()
+        {
+            await File.AppendText("temp.dat").WriteAsync("a");
+            label1.Text = "test";
+        }
+
+        private void UpdatePathList(string[] fileNames)
+        {
+            excellFilePaths.Clear();
+            listbox_excellPaths.Items.Clear();
+            foreach (var path in fileNames)
+            {
+                excellFilePaths.Add(new ExcellFile() { Path = path, FileName = Path.GetFileName(path) });
+            }
+            listbox_excellPaths.Items.AddRange(excellFilePaths.ToArray());
+        }
+
+        private void AddPathList(string[] fileNames)
+        {
+            listbox_excellPaths.Items.Clear();
+            foreach (var path in fileNames)
+            {
+                excellFilePaths.Add(new ExcellFile() { Path = path, FileName = Path.GetFileName(path) });
+            }
+            listbox_excellPaths.Items.AddRange(excellFilePaths.ToArray());
+        }
+
+        private void GetOpenAndFileType(string fileFilter , bool addExcell = false)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = fileFilter;
+            if(addExcell)
+                openFileDialog.FileOk += OpenFileDialog_FileOk2;
+            else
+                openFileDialog.FileOk += OpenFileDialog_FileOk;
+            openFileDialog.ShowDialog();
+        }
+
+        private void OpenFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            if (!e.Cancel)
+            {
+                OpenFileDialog opd = sender as OpenFileDialog;
+                UpdatePathList(opd.FileNames);
+            }
+        }
+
+        private void OpenFileDialog_FileOk2(object sender, CancelEventArgs e)
+        {
+            if (!e.Cancel)
+            {
+                OpenFileDialog opd = sender as OpenFileDialog;
+                AddPathList(opd.FileNames);
+            }
+        }
+
+        private async void ReadAndJoinExcells()
+        {
+            int startingRowIndex = 1;
+            bool isFirst = true;
+            int FileSizeIndex = 0;
+            int maxfileSize = excellFilePaths.Count;
+
+            Application application = null;
+            Workbooks wbooks = null;
+            Worksheet destinationSheet = null;
+            Workbook destinationWorkbook = null;
+            try
+            {
+                WriteConsole("Kopyalama Başlıyor...");
+                await Task.Run(() =>
+                {
+                    //Instantiate the application object
+                    application = new Application();
+                    application.CutCopyMode = (Microsoft.Office.Interop.Excel.XlCutCopyMode)0;
+                    wbooks = application.Workbooks;
+                    object misValue = System.Reflection.Missing.Value;
+
+                    destinationWorkbook = wbooks.Add(misValue);
+                    destinationSheet = (Worksheet)destinationWorkbook.Worksheets.get_Item(1);
+                    //Copy Excel worksheet from source workbook to the destination workbook
+                });
+                foreach (var item in excellFilePaths)
+                {
+                    FileSizeIndex++;
+                    WriteConsole($" {FileSizeIndex}/{maxfileSize} {item.FileName} Kopyalanıyor...");
+                    await Task.Run(() =>
+                    {
+                        Workbook sourceWorkbook = wbooks.Open(item.Path);
+                        Worksheet wsheet = (Worksheet)sourceWorkbook.Worksheets.get_Item(1);
+                        if (isFirst)
+                        {
+                            startingRowIndex = 1;
+                        }
+                        else
+                        {
+                            startingRowIndex = 2;
+                        }
+                        var usedRange = wsheet.UsedRange;
+                        usedRange.Cells[1,1].Copy();
+                        usedRange.Range[usedRange[startingRowIndex, 1], usedRange[usedRange.Rows.Count, usedRange.Columns.Count]].Copy();
+                        if (isFirst)
+                        {
+                            destinationSheet.Paste(destinationSheet.UsedRange.Cells[1, 1]);
+                            isFirst = false;
+                        }
+                        else
+                        {
+                            destinationSheet.Paste(destinationSheet.UsedRange.Cells[destinationSheet.UsedRange.Rows.Count + 1, 1]);
+                        }
+                        sourceWorkbook?.Close();
+                        Marshal.ReleaseComObject(sourceWorkbook);
+                    });
+                }
+
+                WriteConsole("Çıktı oluşturuluyor...");
+                //Save the file
+
+                await Task.Run(() =>
+                {
+                    destinationWorkbook.SaveAs(outFile.Path, AccessMode: XlSaveAsAccessMode.xlShared);
+                });
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                WriteConsole("Çıktın Hazır :) Güle güle kullan...");
+                try
+                {
+                    destinationWorkbook?.Close();
+                    Marshal.ReleaseComObject(destinationWorkbook);
+                    wbooks?.Close();
+                    Marshal.ReleaseComObject(wbooks);
+                    application?.Quit();
+                    Marshal.FinalReleaseComObject(application);
+                }
+                catch (Exception) { }
+                application = null;
+            }
+        }
+
+        private void Btn_select_out_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excell Dosyası |*.xlsx";
+            saveFileDialog.FileOk += SaveFileDialog_FileOk; ;
+            saveFileDialog.ShowDialog();
+        }
+
+        private void WriteConsole(string text)
+        {
+            lstBox_output.Items.Add(text);
+            lstBox_output.SelectedIndex = lstBox_output.Items.Count - 1;
+        }
+        private void ClearConsole()
+        {
+            lstBox_output.Items.Clear();
+        }
+
+        private void SaveFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            if (!e.Cancel)
+            {
+                SaveFileDialog sfd = sender as SaveFileDialog;
+                outFile = new ExcellFile()
+                {
+                    Path = sfd.FileName
+                };
+                lstbox_outPath.Items.Clear();
+                lstbox_outPath.Items.Add(sfd.FileName);
+            }
+        }
+
+        private void Btn_show_totalRowSize_Click(object sender, EventArgs e)
+        {
+            CalculateRowSize();
+        }
+
+        private async void CalculateRowSize()
+        {
+            int row_size = 0;
+
+            Application application = null;
+            Workbooks wbooks = null;
+            Workbook sourceWorkbook = null;
+            try
+            {
+                application = new Application();
+                wbooks = application.Workbooks;
+                listbox_excellPaths.Enabled = false;
+                foreach (ExcellFile item in excellFilePaths)
+                {
+                    if (item.RowSize == 0)
+                    {
+                        WriteConsole($"{item.FileName} Boyutu Okunuyor...");
+                        listbox_excellPaths.SelectedItem = item;
+                        await Task.Run(() =>
+                        {
+                            sourceWorkbook = wbooks.Open(item.Path);
+                            Worksheet wsheet = (Worksheet)sourceWorkbook.Worksheets.get_Item(1);
+                            row_size += wsheet.UsedRange.Rows.Count;
+                            item.RowSize = wsheet.UsedRange.Rows.Count;
+                            sourceWorkbook?.Close();
+                            Marshal.ReleaseComObject(sourceWorkbook);
+                        });
+                        int index = listbox_excellPaths.Items.IndexOf(item);
+                        listbox_excellPaths.Items.Remove(item);
+                        listbox_excellPaths.Items.Insert(index, item);
+                    }
+                    else {
+                        row_size += item.RowSize;
+                    }
+                    SetRowCount(row_size);
+                }
+                listbox_excellPaths.Enabled = true;
+                wbooks?.Close();
+                Marshal.ReleaseComObject(wbooks);
+                application?.Quit();
+                Marshal.ReleaseComObject(application);
+                WriteConsole($"Toplam Satır Boyutu {row_size} olarak okundu.");
+                MessageBox.Show($"{row_size}", "Toplam Satır Sayısı");
+            }
+            catch { }
+            finally
+            {
+                try
+                {
+                    //sourceWorkbook?.Close();
+                    //Marshal.ReleaseComObject(sourceWorkbook);
+
+                }
+                catch (Exception) { }
+                application = null;
+            }
+        }
+
+        private void SetRowCount(int row_Count)
+        {
+            string row_count_str = row_Count.ToString();
+
+         
+            if(row_Count >= 1000000)
+            {
+                lbl_rowCount.ForeColor = Color.Red;
+            }
+            else
+            {
+                lbl_rowCount.ForeColor = Color.Green;
+            }
+
+            lbl_rowCount.Text = row_count_str;
+            Row_Count = row_Count;
+        }
+
+        private void Listbox_excellPaths_KeyDown(object sender, KeyEventArgs e)
+        {
+            System.Windows.Forms.ListBox pathListBox = (System.Windows.Forms.ListBox)sender;
+            ExcellFile selectedItem = pathListBox.SelectedItem as ExcellFile;
+            if (e.KeyCode == Keys.Delete && selectedItem != null)
+            {
+                listbox_excellPaths.Items.Remove(selectedItem);
+                excellFilePaths.Remove(selectedItem);
+                if(selectedItem.RowSize != 0 && Row_Count != 0 )
+                    SetRowCount(Row_Count - selectedItem.RowSize);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            GetOpenAndFileType("Excell Dosyası |*.xlsx" , true);
+        }
+    }
+}
